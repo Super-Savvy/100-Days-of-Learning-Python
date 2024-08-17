efd099a5c239a8ae86543aaf9f2e712c2efdbbc9
Super-Savvy <banjopatrick20@gmail.com>
2024-08-17 03:02:54 +0100

    test practice

--- Day1.py
+++ Day1.py
@@ -1,9 +1,9 @@
 print("""
 Patrick
 August 17th 2023
-I am signing up for Replit's 100 days of Python challenge!
-I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I can be. No excuses for not coding from the middle of a field!
+I am starting Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 15 minutes a day.
+I'll be using Replit (and vscode cause I like vscode), an amazing online IDE so I can do this from my phone wherever I can be. No excuses for not coding from the middle of a field!
 I am feeling 😊
 You can follow my progress at github.com/Super-Savvy
 """)
