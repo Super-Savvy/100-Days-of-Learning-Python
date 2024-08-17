f2701c6549fefb47faa38965278063a54f29e5a4
Super-Savvy <banjopatrick20@gmail.com>
2024-08-17 03:05:45 +0100

    Day One

--- Day1.py
+++ Day1.py
@@ -3,7 +3,7 @@ Patrick
 August 17th 2023
 I am starting Replit's 100 days of Python challenge!
 I will make sure to spend some time every day coding along, for a minimum of 15 minutes a day.
-I'll be using Replit (and vscode cause I like vscode), an amazing online IDE so I can do this from my phone wherever I can be. No excuses for not coding from the middle of a field!
-I am feeling 😊
+I'll be using Replit (and vscode cause I like vscode), an online IDE so I can do this from my phone wherever I can be. No excuses for not coding from the middle of a field!
+I am feeling great 😊
 You can follow my progress at github.com/Super-Savvy
 """)
