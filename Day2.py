2b2cc521d751a6fc5290ad08c033b3c306f2b90a
Super-Savvy <banjopatrick20@gmail.com>
2024-08-18 16:46:57 +0100

    Day Two

---
+++ Day2.py
@@ -0,0 +1,8 @@
+user_name = input("What is your name: ")
+user_age = input("What is your age: ")
+
+print("So your namae is")
+print(user_name)
+print("and you're")
+print(user_age)
+print("that's cool")
