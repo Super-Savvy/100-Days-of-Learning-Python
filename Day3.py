e1e7153d21f03a7d4ca6c66fd2bf4fc76837d98b
Super-Savvy <banjopatrick20@gmail.com>
2024-08-19 19:27:02 +0100

    Day Three

---
+++ Day3.py
@@ -0,0 +1,6 @@
+# today we are working on concatenation
+
+user_name = input("What is your name: ")
+user_lunch_food = input("What are you having for lunch? ")
+
+print("Yo!!", user_name, "you are enjoying", user_lunch_food, "for lunch that is wild I wish I can also have that.")
