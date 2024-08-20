396657345bae8f1cf509e4a343d0aa6909fe3696
Super-Savvy <banjopatrick20@gmail.com>
2024-08-20 23:53:15 +0100

    Day Four

---
+++ Day4.py
@@ -0,0 +1,9 @@
+# Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!
+
+name= input("What is your name? ")
+worst_enemy_name =input("What is your worst enemy's name? ")
+superpower= input("What is your superpower? ")
+location = input("Where do you live? ")
+fav_food = input("What is your favorite food? ")
+
+print("\nHello", "\033[37m",  name,"\033[0m", "Your ability to", "\033[34m", superpower,"\033[0m", "will make sure you never have to look at", "\033[31m",worst_enemy_name,"\033[0m","again. Go eat", "\033[32m",fav_food,"\033[0m","as you walk down the streets of","\033[33m",location,"\033[0m","and use","\033[34m",superpower,"\033[0m","for good and not evil!")
