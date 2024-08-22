44d007cf0631521dea1a9f5a2e7219f7b2b32ac0
Super-Savvy <banjopatrick20@gmail.com>
2024-08-22 21:47:43 +0100

    Day Five

---
+++ Day5.py
@@ -0,0 +1,5 @@
+user_name= input("State your name intruder? ")
+if user_name=="Patrick":
+    print("Welcome Master \nLooking dashing as always")
+else:
+    print("Who on earth are you! \nBegone you unwanted filthy animal!!")    
---
+++ Day6.py
@@ -0,0 +1,10 @@
+print("ENCRYPTED LOGIN")
+user_name=input("Username > ")
+if user_name == "Patrick":
+    print("Welcome" "Master")
+elif user_name == "SuperSavvy":
+    print("Welcome SuperSavvy")
+elif user_name == "Lucy":
+    print ("Welcome back Lucy")
+else:
+    print("Get out\nYou Discombobulated buffon")               
