94ac654094cbcfb7ab3139bb6f8489d8a556f27a
Super-Savvy <banjopatrick20@gmail.com>
2024-08-22 21:53:53 +0100

    Day Six

--- Day6.py
+++ Day6.py
@@ -1,10 +1,11 @@
 print("ENCRYPTED LOGIN")
 user_name=input("Username > ")
-if user_name == "Patrick":
+password=input("Password > ")
+if user_name == "Patrick" and password == "Patrick":
     print("Welcome" "Master")
-elif user_name == "SuperSavvy":
+elif user_name == "SuperSavvy" and password == "SuperSavvy":
     print("Welcome SuperSavvy")
-elif user_name == "Lucy":
+elif user_name == "Lucy" and password == "Lucy":
     print ("Welcome back Lucy")
 else:
-    print("Get out\nYou Discombobulated buffon")               
+    print("Get out\nYou Discombobulated buffon")
